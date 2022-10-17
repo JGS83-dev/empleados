51a83146073e09e18f9dabbdcfa9d98056ae2cc1
JGS <js8215001@gmail.com>
2022-10-16 18:13:18 -0600

    Creacion Modelo Abstracto

---
+++ Models/Abstracta/AbstractModel.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace empleados.Models.AbstractModel
+{
+    public abstract class AbstractModel<T> where T : class
+    {
+        protected registro_empleadosContext ctx = new registro_empleadosContext();
+
+        DbSet<T> dbSet;
+
+        public AbstractModel()
+        {
+            dbSet = ctx.Set<T>();
+        }
+        public virtual int Insert(T entity)
+        {
+            try
+            {
+                dbSet.Add(entity);
+                ctx.SaveChanges();
+                return 1;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        public virtual List<T> List()
+        {
+            var result = from t in dbSet
+                         select t;
+            return result.ToList();
+        }
+
+        public virtual T GetById(Object id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public virtual int Update(T entity, Object id)
+        {
+            try
+            {
+                T old = dbSet.Find(id);
+                if (old != null)
+                {
+                    ctx.Entry(old).CurrentValues.SetValues(entity);
+                    ctx.SaveChanges();
+                    return 1;
+                }
+                return 0;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        public virtual int Remove(Object id)
+        {
+            try
+            {
+                T entity = dbSet.Find(id);
+                if (entity != null)
+                {
+                    dbSet.Remove(entity);
+                    ctx.SaveChanges();
+                    return 1;
+                }
+                return 0;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+    }
+
+}
