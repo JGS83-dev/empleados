48fbf9429282d0b66eb8560dc1e9fd5e4c03a2cb
JGS <js8215001@gmail.com>
2022-10-16 18:12:37 -0600

    Documentacion BDD

---
+++ Base de Datos/script.sql
@@ -0,0 +1,16 @@
+create database registro_empleados
+
+use registro_empleados
+
+create table empleados(
+id_empleado int identity(1,1) primary key,
+nombre varchar(100) not null,
+apellido varchar(100) not null,
+telefono varchar(25) not null,
+correo varchar(100) not null,
+fecha_contratacion date not null)
+
+create table foto_empleado(
+id_foto int identity(1,1) primary key,
+id_usuario int foreign key references empleados(id_empleado) unique not null,
+foto varbinary(max) not null)
